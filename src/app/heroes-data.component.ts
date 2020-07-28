82fe6de423c711133620d45a17978a494fb8b21d
AlynMing <mingo.alyn@gmail.com>
2020-07-28 17:54:01 +0800

    adding services

---
+++ src/app/heroes-data.component.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Strange' },
+  { id: 12, name: 'Thor' },
+  { id: 13, name: 'Hulk' },
+  { id: 14, name: 'Captain America' },
+  { id: 15, name: 'Black Widow' },
+  { id: 16, name: 'Iron Man' },
+  { id: 17, name: 'Spider Man' },
+  { id: 18, name: 'Ant Man' },
+  { id: 19, name: 'Deadpool' },
+  { id: 20, name: 'Hawk Eye' }
+];
